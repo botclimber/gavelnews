26d9ceb78dd7b1d582bbbc626cc7ea57af380046
Daniel Silva <daniel.silva.prg@gmail.com>
2024-03-30 18:16:41 +0000

    idea: create chat for each New

--- services/ChatService/src/index.ts
+++ services/ChatService/src/index.ts
@@ -4,6 +4,7 @@ import {ChatClass} from './controllers/ChatClass';
 // - save in memory last 50 or 100 messages
 // - prevent/filter suspicious messages with links or script/html injection
 // - prevent spam
+// - [IDEA] create a chat for each New
 
 const PORT = 8001;
 const chatService = new ChatClass(PORT);
