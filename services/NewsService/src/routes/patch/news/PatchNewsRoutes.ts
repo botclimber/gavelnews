43b9b5b434ee130288fcacd509108ab095e3783b
botclimber <daniel.silva.prg@gmail.com>
2024-04-10 15:15:14 +0100

    Update PatchNewsRoutes.ts

--- services/NewsService/src/routes/patch/news/PatchNewsRoutes.ts
+++ services/NewsService/src/routes/patch/news/PatchNewsRoutes.ts
@@ -6,17 +6,21 @@ const PatchNewsRouter = express.Router();
 
 PatchNewsRouter.patch("/:newId/:opinion", (req: Request, res: Response) => {
 
-    const ip = req.ip
+    const userInfo = req.userInfo
+    
     const newId = req.params.newId
     const opinion = req.params.opinion as opinion
 
-    jsonData.updateNewVeracity(newId, opinion, ip)
+    if(userInfo){
+    jsonData.updateNewVeracity(newId, opinion, userInfo)
         .then(response => {
 
             if (response !== undefined) res.status(200).json({ "new_data": response, "allData": jsonData });
             else res.status(401).json({ "msg": "new not found in our db." })
         })
         .catch(e => { console.log(e.message); res.status(400).json({ "msg": e.message }) })
+    
+    }else return res.status(403).json({ "msg": "only allowed users can vote." })
 })
 
 export default PatchNewsRouter;
