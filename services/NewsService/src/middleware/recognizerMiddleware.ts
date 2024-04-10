3ed214ba3ea3381c756bd3ed48ea0a6d65d7507d
botclimber <daniel.silva.prg@gmail.com>
2024-04-10 15:05:17 +0100

    checking if token is being sent within the request

--- services/NewsService/src/middleware/recognizerMiddleware.ts
+++ services/NewsService/src/middleware/recognizerMiddleware.ts
@@ -1,18 +1,34 @@
 import { Request, Response, NextFunction } from 'express';
 import { UsersUtils } from '../../../CommonStuff/src/controllers/UsersUtils';
+import { GoogleAuth  } from '../../../CommonStuff/src/controllers/GoogleAuthUtils';
 
 const userUtils = new UsersUtils()
+const googleAuth = new GoogleAuth()
 
 const loggingMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const ip = req.ip;
-    console.log(`Request from IP: ${ip}`);
+    const checkHeaderAuthForToken = (headerAuth: string) => {
+        if(headerAuth) return headerAuth.trim()
+        else undefined
+    }
+    
+    const userIdentifier = {ip: req.ip ?? "", userAgent: req.headers['user-agent'] ?? ""};
+    const token = checkHeaderAuthForToken(req.headers['authorization']);
+    const userInfo = (token)? await googleAuth.checkGoogleToken(token) : undefined;
+    
+    console.log(`Request from IP: ${userIdentifier.ip}`);
+    console.log(`Request from UserAgent: ${userIdentifier.userAgent}`);
     console.log(`Request Endpoint: ${req.path}`);
 
     try {
-        const userBlocked = await userUtils.checkRemoveExpiredBlock(ip ?? "")
+        const userBlocked = await userUtils.checkRemoveExpiredBlock(userIdentifier, userInfo)
 
         if (userBlocked !== undefined && userBlocked.block.status) return res.status(403).json({ msg: `Sry but you are blocked. timeout until ${userBlocked.block.time}` });
-        else next();
+        else{
+
+            req.userIdentifier = userIdentifier;
+            req.userInfo = userInfo;
+            next();
+        }
 
     } catch (e) {
         console.log(e)
