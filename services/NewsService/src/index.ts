ec969ee37dad70a0e1e3cfab818f0ba7129b4b42
botclimber <daniel.silva.prg@gmail.com>
2024-04-24 15:04:44 +0100

    adapting index.ts to handle both protocols http and https

--- services/NewsService/src/index.ts
+++ services/NewsService/src/index.ts
@@ -1,63 +1,16 @@
-
-import express, { NextFunction, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import cors from "cors";
-import path from "path";
-import http from "http";
-
-import PutAdminRouter from './routes/put/admin/PutAdminRoutes';
-import GetAdminRouter from './routes/get/admin/GetAdminRoutes';
-import GetNewsRouter from './routes/get/news/GetNewsRoutes';
-import GetChatRouter from './routes/get/chat/GetChatRoutes';
-import PostNewsRouter from './routes/post/news/PostNewsRoutes';
-import PatchNewsRouter from './routes/patch/news/PatchNewsRoutes';
-import loggingMiddleware from './middleware/recognizerMiddleware';
-import { checkHeader } from './middleware/AdminMiddleware';
-import { allUsers } from '../../CommonStuff/src/controllers/UsersUtils';
-import { changeDay, persistSensitiveData } from './cron/cronJob';
-import { ChatClass } from './controllers/Chat/ChatClass';
-
-const viewsPath = "../../../../../views/lowLatencyMode/"
-
-const app = express();
-const server = http.createServer(app)
+import createServer from "./server"
+import ServerConfig from '../../CommonStuff/src/types/types';
 
 const PORT = process.env.SERVER_PORT || 80;
 
-// Configure CORS
-app.use(cors());
+const serverConfig: ServerConfig = {
+    useHTTPS: process.env.USE_HTTPS || false,
+    certPath: process.env.CERT_PATH,
+    keyPath: process.env.KEY_PATH
+};
 
-// Parse JSON bodies
-app.use(bodyParser.json());
+const server = createServer(serverConfig);
 
-// Serve static files
-app.use(express.static(path.join(__dirname, viewsPath)));
-
-// Routes
-app.get("/", async function (req: Request, res: Response) {
-    res.sendFile(path.join(__dirname, viewsPath, "index.html"));
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
-
-app.use("/news", loggingMiddleware, GetNewsRouter);
-app.use("/news", loggingMiddleware, PostNewsRouter);
-app.use("/news", loggingMiddleware, PatchNewsRouter);
-app.use("/chat", loggingMiddleware, GetChatRouter);
-
-// Apply authorization check middleware for admin routes
-app.use("/admin", checkHeader, GetAdminRouter);
-app.use("/admin", checkHeader, PutAdminRouter);
-
-// setup chat service
-const chatService = new ChatClass(server);
-
-// Initialize users
-allUsers.setUsers();
-
-// Start schedulers
-persistSensitiveData();
-changeDay(chatService);
-
-server.listen(PORT, function () {
-    console.log("Server listening on port " + PORT);
-});
-
